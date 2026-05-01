cd949de006576f40f9a5718e35872dffea3cfaa4
Usman Paracha <usmannawaz990@gmail.com>
2026-05-01 00:43:01 -0400

    serve frontend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from overbought_oversold_tool import analyze_overbought_oversold
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
@@ -10,3 +11,5 @@ def home():
 @app.get("/analyze")
 def analyze(symbol: str):
     return analyze_overbought_oversold(symbol)
+
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")

72dd6002f20cf326a2d327d76ce3a7345de6f797
Usman Paracha <usmannawaz990@gmail.com>
2026-05-01 00:46:09 -0400

    remove root route

--- main.py
+++ main.py
@@ -4,10 +4,6 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Trading Tools API running"}
-
 @app.get("/analyze")
 def analyze(symbol: str):
     return analyze_overbought_oversold(symbol)
